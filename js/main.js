29dacb769b6a0f8d620fbeb8cb6b95f8de8e4d5e
unknown <m0602227@VW-ECLIPSE-007.pk.amk.root>
2015-04-23 16:37:57 +0300

    alignmed & spinner

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
 $(function() {
     var omdbUrl = "http://www.omdbapi.com/";
     $('.svg').inlineSVG();
+    
     $('form').submit(function(event){
         event.preventDefault();
+        $("#circularG").show();
         $.ajax({
             url: omdbUrl,
             type: 'GET',
             data: {
-                t: $('#input-search').val(),
+                t: $('#input-search').val()               ,
                 plot: 'short',
                 r: 'json'
             },
             success: function(data) {
-                console.log(data);
-                $('#movie h1').append(data.Title);
+                $("#circularG").hide();
+                $('#movie h1').append(data.Title.toUpperCase());
                 $('#movie img').attr('src', data.Poster);
             }
         })
     })
-
 });
+

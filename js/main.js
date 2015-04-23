57314a2a3c145f34589336a231c3fb118cc5bd64
unknown <m0602227@VW-ECLIPSE-007.pk.amk.root>
2015-04-23 15:23:32 +0300

    form & getting movie from omdb api

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
-
 $(function() {
+    var omdbUrl = "http://www.omdbapi.com/";
     $('.svg').inlineSVG();
+    $('form').submit(function(event){
+        event.preventDefault();
+        $.ajax({
+            url: omdbUrl,
+            type: 'GET',
+            data: {
+                t: $('#input-search').val(),
+                plot: 'short',
+                r: 'json'
+            },
+            success: function(data) {
+                console.log(data);
+                $('#movie h1').append(data.Title);
+                $('#movie img').attr('src', data.Poster);
+            }
+        })
+    })
 
 });

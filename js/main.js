2161d18fc7c8b3136ec56c68c0f294c534165277
Markus Juopperi <markus.juopperi@kolumbus.fi>
2015-04-27 18:53:24 +0300

    Use The Movie DB API instead of OMDB

--- js/main.js
+++ js/main.js
@@ -1,27 +1,71 @@
 $(function() {
-    var omdbUrl = "http://www.omdbapi.com/";
-    var movieName = $("#movie h1");
-    var moviePoster = $('#movie #poster');
+    var tmdbUrl = "https://api.themoviedb.org/3";
+    var imageUrl = "https://image.tmdb.org/t/p";
+    var posterWidth = "/w500";
+    var backdropWidth = "/original";
+
+    var movie = $("#movie");
+    var error = $("#error");
     $('.svg').inlineSVG();
-    
+
+    var results;
+
+    function backdropUrl(path) {
+        return imageUrl + backdropWidth + path;
+    }
+
+    function posterUrl(path) {
+        return imageUrl + posterWidth + path;
+    }
+
+    function showError(errorText) {
+        error.find("p").text(errorText);
+        error.show();
+        error.delay(3000).fadeOut(200);
+    }
+
+    function renderNotFound() {
+        showError("Could not find movie. Please try again.")
+    }
+
+    function renderMovie(data) {
+        $("#ajax-loader").hide();
+        results = data.results;
+        console.log("data:", data);
+        var firstResult = _.first(results);
+        if (typeof firstResult != 'undefined') {
+            movie.find("h1").append(firstResult.title);
+            // TODO: Check if poster exists and show placeholder if not
+            var moviePoster = movie.find("img");
+            moviePoster.attr("src", posterUrl + "/w" + posterWidth + firstResult.poster_path);
+            moviePoster.fadeIn(200);
+        } else renderNotFound();
+        $('#input-search').val("");
+    }
+
+    function renderError() {
+        $("#ajax-loader").hide();
+        showError("Movie search failed.");
+    }
+
+    function clearMovies() {
+        movie.find("img").hide();
+        movie.find("h1").text("");
+    }
+
     $('form').submit(function(event){
-        movieName.html("");
-        moviePoster.attr("src", "");
+        clearMovies();
         event.preventDefault();
         $("#ajax-loader").show();
         $.ajax({
-            url: omdbUrl,
+            url: tmdbUrl + "/search/movie",
             type: 'GET',
             data: {
-                t: $('#input-search').val(),
-                plot: 'short',
-                r: 'json'
+                query: $('#input-search').val(),
+                api_key: "3234cd735a781c6bc9cb637e5dad070d"
             },
-            success: function(data) {
-                $("#ajax-loader").hide();
-                movieName.append(data.Title.toUpperCase());
-                moviePoster.attr('src', data.Poster);
-            }
+            success: renderMovie,
+            error: renderError
         })
     })
 });

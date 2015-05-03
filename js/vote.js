ab5f251fafd342b8c1db074611929ce7dfa2f5f7
Markus Juopperi <markus.juopperi@kolumbus.fi>
2015-05-03 22:07:08 +0300

    Prompt for name if not previously given

--- js/vote.js
+++ js/vote.js
@@ -1,6 +1,8 @@
 $(function() {
     $(".svg").inlineSVG();
 
+    var usersRef = new Firebase("https://watchshouldi.firebaseio.com/users");
+
     var chars = 120;
     $("#remainingChars").html(chars);
 
@@ -10,5 +12,26 @@ $(function() {
 
         $("#remaining-chars").html(charsRemaining);
     });
+
+    function createUser(username) {
+        var userRef = usersRef.push({
+            name: username
+        });
+        localStorage.userid = userRef.key();
+        localStorage.username = username;
+    }
+
+    function userExists() {
+        return typeof localStorage.username != "undefined"
+    }
+
+    if (!userExists()) {
+        $("#modal").css('visibility', 'visible');
+        $("#name-form").submit(function (event) {
+            event.preventDefault();
+            createUser($("#username").val());
+            $("#modal").fadeOut(500);
+        });
+    }
 });
 

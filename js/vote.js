d5cf8fbd47eff19de3338e1f10e0f285a98ac719
Sergei Liski <sergeiliski@gmail.com>
2015-05-04 00:47:45 +0300

    added commenting

--- js/vote.js
+++ js/vote.js
@@ -1,15 +1,20 @@
 $(function() {
     $(".svg").inlineSVG();
 
+    var url = window.location.href;
+    var voteId = url.replace("https://watchshouldi.firebaseio.com/votes/", "");
     var usersRef = new Firebase("https://watchshouldi.firebaseio.com/users");
-
+    voteId = "7YNzvy";
+    var commentsRef = new Firebase("https://watchshouldi.firebaseio.com/votes/" + voteId + "/comments");
     var chars = 120;
+    var charsRemaining;
+
+    getComments(commentsRef);
+
     $("#remainingChars").html(chars);
-
-    $("textarea").keydown(function() {
+    $("textarea").keyup(function() {
         var textLength = $("textarea").val().length;
-        var charsRemaining = chars - textLength;
-
+        charsRemaining = chars - textLength;
         $("#remaining-chars").html(charsRemaining);
     });
 
@@ -33,5 +38,27 @@ $(function() {
             $("#modal").fadeOut(500);
         });
     }
+
+    $("#comments-container form").submit(function(event) {
+        event.preventDefault();
+        commentsRef.push({ 'username': localStorage.username, 'comment': $("#input-comment").val(), 'time': moment().format("dddd, MMMM Do YYYY, h:mm:ss a") });
+        $("#input-comment").val("");
+        charsRemaining = chars;
+        $("#remaining-chars").html(charsRemaining);
+    })
 });
+
+function getComments(commentsRef) {moment([2007, 0, 29]).fromNow()
+    commentsRef.orderByChild("comments").on("child_added", function(data) {
+        $("#comments").prepend('<div class="comment-box">' +
+            '<div class="comment-head">' +
+            '<div class="comment-name">' + data.val().username + '<span>' + data.val().time + '</span></div>' +
+            '</div>' +
+            '<div class="comment-content">' + data.val().comment + '</div>' +
+            '</div>'
+        )
+        $(".comment-box").slideDown(200);
+    })
+}
+
 
